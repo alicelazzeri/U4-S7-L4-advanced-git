ed2bc098f911875547a7eec262c4ccaecdc4be6f
Alice Lazzeri <alice.lazzeri@hotmail.it>
2024-05-30 16:07:45 +0200

    first commit on develop branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("First commit on main branch");
+        System.out.println("First commit on develop branch");
 
     }
 }
