491e49212cb9017c333418b70497f85672e2af85
Alice Lazzeri <alice.lazzeri@hotmail.it>
2024-05-30 16:16:24 +0200

    first commit on test branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("First commit on main branch");
+        System.out.println("First commit on test branch");
 
     }
 }
