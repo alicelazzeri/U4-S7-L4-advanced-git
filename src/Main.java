e6d3bc054a0df75ac0794069fa6840ac4dad038d
Alice Lazzeri <alice.lazzeri@hotmail.it>
2024-05-30 16:09:55 +0200

    trying to merge develop into main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("First commit on develop branch");
+        System.err.println("Trying to merge this content on main branch");
 
     }
 }
